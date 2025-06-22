83c51a143dc7df5bd6790d861d2902357cd732e0
Call Vin <kanggara75@gmail.com>
2025-06-22 22:38:40 +0700

    add: stocks.sql

--- stocks.sh
+++ stocks.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-input="all.csv"
+url="https://raw.githubusercontent.com/KAnggara75/Dataset-Saham-IDX/refs/heads/master/List%20Emiten/all.csv"
 output="stocks.sql"
 schema="idxstock"
 table="stocks"
 
-# Buat DDL opsional (hapus jika tidak perlu)
 cat <<EOF >"$output"
 INSERT INTO $schema.$table (code, name, listing_date, shares, board) VALUES
 EOF
 
-tail -n +2 "$input" | while IFS=',' read -r code name listingDate shares listingBoard; do
+curl -sL "$url" | tail -n +2 | while IFS=',' read -r code name listingDate shares listingBoard; do
 	date_formatted=$(echo "$listingDate" | sed 's/T.*//')
 	shares_formatted=$(echo "$shares" | sed 's/\.0$//')
 	echo "('$code', '$name', '$date_formatted', $shares_formatted, '$listingBoard')," >>"$output"
