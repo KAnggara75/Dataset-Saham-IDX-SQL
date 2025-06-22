22677eae01054b829ca888fc26486998d0a65a4a
Call Vin <kanggara75@gmail.com>
2025-06-22 22:49:26 +0700

    add: history.sh

--- history.sh
+++ history.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-search_dir=csv2sql
-filename=
+set -e
+
+search_dir="csv2sql"
+output_dir="history"
 
 abort() {
 	echo "$@"
@@ -9,51 +11,61 @@ abort() {
 }
 
 copy_file() {
-	cp ./Saham/*.csv ./csv2sql/
+	mkdir -p "$search_dir"
+	cp ./Saham/*.csv "$search_dir"/
 }
 
 read_all_file() {
+	mkdir -p "$output_dir"
 	for file in "$search_dir"/*.csv; do
-		filename=$(echo "$file" | sed "s/\.\///g")
-		old_filename=$(echo "$filename" | sed "s/$search_dir\///g")
-		# shellcheck disable=SC2001
-		new_filename=$(echo "$old_filename" | sed "s/\.csv//g")
-		echo "Formating, $old_filename to $new_filename.sql"
-		# Replace blank with NULL
-		sed -i='' -e 's/,,/,NULL,/g' "$filename"
-		# Remove T00:00:00 if exist
-		sed -i='' -e "s/T00:00:00//g" "$filename"
-		# add quote 'YYYY-MM-DD'
-		sed -i='' -e 's/\([0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\)/'\''\1'\''/' "$filename"
-		# add (
-		sed -i='' -e "1!s/^/('$new_filename',/" "$filename"
-		# add ),
-		sed -i='' -e '1!s/$/),/' "$filename"
-		# Remove coma symbol on last line
-		sed -i='' -e '$s/,$//' "$filename"
-		# add INSERT (column... in first Line
-		sed -i='' -e '1s/^/INSERT INTO history (code, /' "$filename"
-		# add ) VALUES in first Line
-		sed -i='' -e '1s/$/) VALUES/' "$filename"
-		# fix change to `change`
-		# shellcheck disable=SC2016
-		sed -i='' -e 's/change/`change`/g' "$filename"
+		filename="${file#./}"                     # Hilangkan './' di depan
+		old_filename="${filename#${search_dir}/}" # Ambil nama file saja
+		new_filename="${old_filename%.csv}"       # Hilangkan ekstensi .csv
 
-		mv $search_dir/"$old_filename" ./history/"$new_filename".sql
+		echo "Formating $old_filename to $new_filename.sql"
+
+		# Ganti kosong dengan NULL
+		sed -i='' -e 's/,,/,NULL,/g' "$filename"
+
+		# Hapus T00:00:00 jika ada
+		sed -i='' -e 's/T00:00:00//g' "$filename"
+
+		# Bungkus tanggal YYYY-MM-DD dengan tanda petik satu
+		sed -i='' -e "s/\([0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\)/'\1'/" "$filename"
+
+		# Tambahkan ('CODE', di setiap baris kecuali baris pertama (header)
+		sed -i='' -e "1!s/^/('$new_filename',/" "$filename"
+
+		# Tambahkan ), di akhir setiap baris kecuali baris pertama
+		sed -i='' -e "1!s/\$/),/" "$filename"
+
+		# Hapus koma di akhir baris terakhir
+		sed -i='' -e '$s/,$//' "$filename"
+
+		# Tambahkan header INSERT ... pada baris pertama
+		sed -i='' -e '1s/^/INSERT INTO history (code, /' "$filename"
+
+		# Tambahkan ) VALUES pada akhir baris pertama
+		sed -i='' -e '1s/\$/) VALUES/' "$filename"
+
+		# Ganti kata change dengan `change`
+		sed -i='' -e 's/\bchange\b/`change`/g' "$filename"
+
+		mv "$file" "$output_dir/$new_filename.sql"
 	done
 }
 
 main() {
 	clear
 	pwd
-	mkdir -p csv2sql
-	rm -rf history
-	mkdir -p history
+	rm -rf "$output_dir"
+	mkdir -p "$output_dir"
+	mkdir -p "$search_dir"
 
 	copy_file
 	read_all_file
 
-	rm -rf csv2sql
+	rm -rf "$search_dir"
 }
 
 main || abort "Compose Error!"
